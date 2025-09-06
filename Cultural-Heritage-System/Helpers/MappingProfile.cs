c45a9481f89a1a01a404247305361401b912db81
admin <admin@LAPTOP-56OHGFRG>
2025-09-06 14:02:35 +0700

    commit

--- Cultural-Heritage-System/Helpers/MappingProfile.cs
+++ Cultural-Heritage-System/Helpers/MappingProfile.cs
@@ -136,8 +136,17 @@ namespace Cultural_Heritage_System.Helpers
                 .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Heritage.Category.Name))
                 .ForMember(dest => dest.IsFeatured, opt => opt.MapFrom(src => src.Heritage.IsFeatured))
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt));
+
             CreateMap<ContributionCreationRequest, Contribution>();
-            CreateMap<Contribution, ContributionCreationResponse>();
+            CreateMap<Contribution, ContributionResponse>()
+             .ForMember(d => d.ContributorName, o => o.MapFrom(s => s.Contributor.User.UserName))
+             .ForMember(d => d.AvatarUrl, o => o.MapFrom(s => s.Contributor.User.Profile.AvatarUrl));         
+            CreateMap<Contribution, ContributionSearchResponse>()
+             .ForMember(d => d.ContributorName, o => o.MapFrom(s => s.Contributor.User.UserName))
+             .ForMember(d => d.AvatarUrl, o => o.MapFrom(s => s.Contributor.User.Profile.AvatarUrl))
+             .ForMember(d => d.PostedAt, o => o.MapFrom(s => s.UpdatedAt))
+             .ForMember(d => d.ContributorNameUnsigned, o => o.MapFrom(s => s.Contributor.User.UserNameUnsigned))
+             .ForMember(d => d.FirstText, o => o.MapFrom<ContributionFirstTextResolver>());
         }
     }
 }
