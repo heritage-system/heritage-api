11f4eb9cbd0768588477962e90bb504f7cbf31fc
admin <admin@LAPTOP-56OHGFRG>
2025-12-03 11:38:44 +0700

    commit all

--- Cultural-Heritage-System/Services/IMailService.cs
+++ Cultural-Heritage-System/Services/IMailService.cs
@@ -6,6 +6,7 @@
         Task SendEmailOtpResetPassword(string to,string otp, DateTime expiresAt);
         Task SendEmailAnswerReport(string to, string userName, string heritageName, string reportTime, string reportContent, string replyMessage);
         Task SendEmailWelcomeForAdmin(string to, string user, string password, string role, string username);
+        Task SendRemindEmail(string to, string userName, string eventName, string startTime, string eventDate, string joinUrl, DateTime scheduleTimeUtc);
     }
 }
 
